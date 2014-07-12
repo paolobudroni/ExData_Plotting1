9fa29e5ff4ef6a963aaa3e32425a3f7133f49380
paolobudroni <paolo.budroni@tiscali.it>
2014-07-12 17:04:45 +0200

    Course Project1

--- plot4.R
+++ plot4.R
@@ -16,7 +16,7 @@ plot4 <- function() {
 	# check device 
 	if (dev.cur() == 1) dev.new()	
 
-	par(mfrow=c(2,2),oma=c(1,1,1,1),bg="white")
+	par(mfrow=c(2,2),oma=c(0,1,0,0),bg="white")
 
 	with(hpcdatafilt, {
 		plot(datetime,Global_active_power, type="l" ,xlab="", ylab="Global Active Power")
